1a6388552cab364024e262a8c2abbf20ffa3756a
CnaspHuangxl <102742232+CnaspHuangxl@users.noreply.github.com>
2022-04-19 15:45:22 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include"ThreadPool.h"
+#include"unistd.h"
+#include<iostream>
+void taskFunc(void* arg)
+{
+    int num = *(int*)arg;
+    cout<<"thread" <<pthread_self()<<"is working, number = "<<num<<endl;
+    sleep(1);
+}
+
+int main()
+{
+    ThreadPool pool(3, 10);
+    // 创建线程池
+    for (int i = 0; i < 100; ++i)
+    {
+        int* num = new int(i+100);
+        pool.addTask(Task(taskFunc, num));
+    }
+
+    sleep(20);
+
+    return 0;
+}
+
